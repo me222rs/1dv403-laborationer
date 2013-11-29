db12860a68ad106aa24289042866b3ddb5e2af94
OpenShift guest <me222rs@student.lnu.se>
2013-11-29 10:49:35 -0500

    Påbörjat kodningen lite grann

---
+++ 2-labbymezzage/MessageBoard.js
@@ -0,0 +1,14 @@
+function MessageBoard(boardID){
+    var messages = [];          //Array som ska hålla alla meddelande
+}
+
+var mess = new Message("Testmeddelande", new Date());
+alert(mess);
+alert(mess.getText());
+mess.setText("En annan text");
+alert(mess);
+
+
+window.onload = function(){     
+    new MessageBoard();         //Denna metod kommer att köras när sidan har laddats klart
+};
