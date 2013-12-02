21c6cc71ca14ea241eaf8fb7a0c9987200327b8f
OpenShift guest <me222rs@student.lnu.se>
2013-12-02 05:47:46 -0500

    Skrivit om lite av koden

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,14 +1,20 @@
-function MessageBoard(boardID){
-    var messages = [];          //Array som ska hålla alla meddelande
-}
-
-var mess = new Message("Testmeddelande", new Date());
-alert(mess);
-alert(mess.getText());
-mess.setText("En annan text");
-alert(mess);
-
-
-window.onload = function(){     
-    new MessageBoard();         //Denna metod kommer att köras när sidan har laddats klart
+var MessageBoard = {        // Statiskt objekt
+    
+    messages: [],           // Array som egenskap kommer att hålla alla meddelanden
+    
+    init:function(e){  
+        
+        var messageButton = document.getElementById("button");      //Denna kod letar reda på knappen i html dokumentet
+        var inputMessage = document.getElementById("messageboard"); //Denna kod letar reda på den inmatade texten
+        messageButton.onclick = MessageBoard.addNewMessage;            //Denna kod kopplar ett event till skriv-knappen
+        console.log(inputMessage);
+        
+    },
+    
+    addNewMessage: function(){
+        //Skapa ett nytt meddelandeobjekt med datum och meddelande typ?
+    }
 };
+
+
+window.onload = MessageBoard.init;             //Denna metod kommer att köras när sidan har laddats klart
